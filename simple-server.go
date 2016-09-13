4d5a92a4302d745eafecf1033ec8796660aa2647
harryge00 <harryge00@gmail.com>
2016-09-14 00:12:38 +0800

    simple server

---
+++ simple-server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"fmt"
+)
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	fmt.Println(r.Header)
+	body, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(body))
+	io.WriteString(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8000", nil)
+}
